3229faf22ffbb07dbfb64da3d9a0772256ac6951
Aviral Garg <contact@aviralgarg.com>
2020-03-12 12:43:09 -0700

    speech button in tab4

--- app/src/main/java/com/example/voice_code/tabComposables/Tab4.kt
+++ app/src/main/java/com/example/voice_code/tabComposables/Tab4.kt
@@ -2,7 +2,12 @@ package com.example.voice_code.tabComposables
 
 
 import androidx.compose.Composable
+import androidx.compose.Model
 import androidx.ui.core.Text
+import androidx.ui.graphics.Color
+import androidx.ui.material.Button
+import androidx.ui.material.ButtonStyle
+import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
 import com.example.voice_code.DefaultPreview
 import com.example.voice_code.dataProviders.Tabs
@@ -13,8 +18,21 @@ fun DefaultPreview_Tab4() {
     DefaultPreview(Tabs.Tab4)
 }
 
+@Model
+class ListeningState(var isListening: Boolean = false)
+
 @Composable
-fun Tab4() {
-    // TODO: Implement Tab4
-    Text(text = "Tab4: Not yet Implemented")
+fun Tab4(
+    state: ListeningState = ListeningState(true)
+) {
+    Button(
+        text = "Speech",
+        onClick = { state.isListening = !state.isListening },
+        style = ButtonStyle(
+            backgroundColor = if (state.isListening) Color.Green else Color.White,
+            contentColor = Color.Black,
+            shape = (MaterialTheme.shapes()).button
+        )
+
+    )
 }
